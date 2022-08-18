adf6e243c180b83526c67264b9e4381e8d723942
Rick1124 <38147073+Rick1124@users.noreply.github.com>
2022-08-18 01:47:46 -0500

    Final

--- src/com/tema3/Main.java
+++ src/com/tema3/Main.java
@@ -1,2 +1,12 @@
-package com.tema3;public class Main {
+package com.tema3;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] nombres = {"Juan", "Pepe", "Toño", "Lalo"};
+        String concat = "";
+        for (String nombre : nombres) {
+            concat += " " + nombre;
+        }
+        System.out.println(concat);
+    }
 }
